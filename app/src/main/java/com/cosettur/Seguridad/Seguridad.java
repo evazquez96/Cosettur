ea000b39bee0fd8dd397e38e71537fdaaea0d83a
evazquez96 <evazquez@logsys.com.mx>
2018-08-01 02:42:12 -0500

    Se creo el paquete com.cosettur.Seguridad dentro de la cual servira para encriptar las contraseñas de los usuarios con md5

---
+++ app/src/main/java/com/cosettur/Seguridad/Seguridad.java
@@ -0,0 +1,13 @@
+package com.cosettur.Seguridad;
+
+public class Seguridad extends Object {
+
+    /**
+     * La clase Seguridad servira para encriptar la contraseña
+     * de los usuarios.
+     */
+
+    public static String encriptarMD5(String password){
+        return null;
+    }
+}
