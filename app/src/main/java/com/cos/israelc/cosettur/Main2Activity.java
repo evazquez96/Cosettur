54c3189b39f9aced81ca7d2d7c098c664fbe9a22
icanul <icanul@logsys.com.mx>
2018-10-07 01:00:59 -0500

    correo

--- app/src/main/java/com/cos/israelc/cosettur/Main2Activity.java
+++ app/src/main/java/com/cos/israelc/cosettur/Main2Activity.java
@@ -1,11 +1,17 @@
 package com.cos.israelc.cosettur;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Properties;
+
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -15,41 +21,37 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.os.AsyncTask;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-public class pages extends Activity implements OnClickListener{
-
+public class Main2Activity extends AppCompatActivity {
     Session session = null;
+    EditText reciep, sub, msg;
     ProgressDialog pdialog = null;
     Context context = null;
-    EditText reciep, sub, msg;
+    Button login;
     String rec, subject, textMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        context = this;
-
-        Button login = (Button) findViewById(R.id.btn_submit);
+        setContentView(R.layout.activity_main2);
+        login = (Button) findViewById(R.id.btn_submit);
         reciep = (EditText) findViewById(R.id.et_to);
         sub = (EditText) findViewById(R.id.et_sub);
         msg = (EditText) findViewById(R.id.et_text);
+        context = this;
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-     //   login.setOnClickListener(this);
+         validar();
+
+            }
+        });
     }
-
-    @Override
-    public void onClick(View v) {
+    public void secion(){
         rec = reciep.getText().toString();
+        subject = sub.getText().toString();
+        textMessage = msg.getText().toString();
 
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
@@ -59,17 +61,16 @@ public class pages extends Activity implements OnClickListener{
         props.put("mail.smtp.port", "465");
 
         session = Session.getDefaultInstance(props, new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("cosettur.soporte@gmail.com", "cosetturapps123");
-            }
-        });
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("cosettur.soporte@gmail.com", "cosetturapps123");
+                    }
+                }
 
+        );
         pdialog = ProgressDialog.show(context, "", "Sending Mail...", true);
-
         RetreiveFeedTask task = new RetreiveFeedTask();
         task.execute();
     }
-
     class RetreiveFeedTask extends AsyncTask<String, Void, String> {
 
         @Override
@@ -79,8 +80,8 @@ public class pages extends Activity implements OnClickListener{
                 Message message = new MimeMessage(session);
                 message.setFrom(new InternetAddress("testfrom354@gmail.com"));
                 message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(rec));
-                message.setSubject("Recuperacion de contraseña");
-                message.setContent("Gracias por Usar CosetturApp  de clic en el seguiente enlace para recuperar su contraseña "+"/n"+"https://sites.google.com/view/recuperacioncosettur/p%C3%A1gina-principal", "text/html; charset=utf-8");
+                message.setSubject("recuperacion de contraseña");
+                message.setContent("https://sites.google.com/view/recuperacioncosettur/p%C3%A1gina-principal?authuser=1", "text/html; charset=utf-8");
                 Transport.send(message);
             } catch(MessagingException e) {
                 e.printStackTrace();
@@ -99,4 +100,24 @@ public class pages extends Activity implements OnClickListener{
             Toast.makeText(getApplicationContext(), "Message sent", Toast.LENGTH_LONG).show();
         }
     }
+    public void validar(){
+if(reciep.getText().toString().equals("")){
+    reciep.setHint("campo obligatorio");
+
+}else if(sub.getText().toString().equals("")){
+    sub.setHint("campo obligatorio");
+}else if(reciep.getText().toString().equals(sub.getText().toString())){
+    secion();
+
+}else {
+    reciep.setHint("No coinciden los campos");
+   sub.setHint("No coinciden los campos");
+}
+{
+
+}
+
+    }
+
+
 }
